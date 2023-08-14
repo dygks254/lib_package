5bf26572949ff04e9e9bdfb5ddbc03f463294120
YohabPark <dyqkr@naver.com>
2023-08-15 03:29:11 +0900

    Reset python flake

--- unitTest/unitTest_scripts/unitTest.py
+++ unitTest/unitTest_scripts/unitTest.py
@@ -1,5 +1,6 @@
 import unittest
-import os, sys
+import os
+import sys
 
 '''
 Unittest python script
@@ -11,24 +12,25 @@ sys.path.append(GIT_TOP)
 
 import pythonPack
 
+
 class SampleTest(unittest.TestCase):  
 
-  def test_defaultPrint(cls):
-    print("==This is default unittest==")
-    print(f"\t checking file absPath :: {THIS_DIR}")
-    print(f"\t checking file absPath :: {GIT_TOP}")
+    def test_defaultPrint(cls):
+        print("==This is default unittest==")
+        print(f"\t checking file absPath :: {THIS_DIR}")
+        print(f"\t checking file absPath :: {GIT_TOP}")
 
-  def test_checkPath(cls):
-    print("==Test checkPath scripts==")
-    print(f"\t checking directory absPath :: {pythonPack.checkPath.validPathType( os.path.join(GIT_TOP, 'unitTest/unitTest_build/checkPath'))}")
-    print(f"\t checking file absPath :: {pythonPack.checkPath.validFileType( os.path.join(GIT_TOP, 'unitTest/unitTest_build/checkPath/test.txt'))}")
-    try:
-      pythonPack.checkPath.validPathType( os.path.join(GIT_TOP, 'unitTest/unitTest_build/checkPath'))
-      pythonPack.checkPath.validPathType( os.path.join(GIT_TOP, 'unitTest/unitTest_build/checkPath/text2.txt'))
-      raise ValueError
-    except:
-      pass
-      
+    def test_checkPath(cls):
+        print("==Test checkPath scripts==")
+        print(f"\t checking directory absPath :: {pythonPack.checkPath.validPathType( os.path.join(GIT_TOP, 'unitTest/unitTest_build/checkPath'))}")
+        print(f"\t checking file absPath :: {pythonPack.checkPath.validFileType( os.path.join(GIT_TOP, 'unitTest/unitTest_build/checkPath/test.txt'))}")
+        try:
+            pythonPack.checkPath.validPathType(os.path.join(GIT_TOP, 'unitTest/unitTest_build/checkPath'))
+            pythonPack.checkPath.validPathType(os.path.join(GIT_TOP, 'unitTest/unitTest_build/checkPath/text2.txt'))
+            raise ValueError
+        except Exception:
+            pass
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
