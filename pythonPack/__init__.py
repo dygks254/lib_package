ee18fe50ec9565507f66ef8b83cf846151c3d2f2
YohabPark <dyqkr@naver.com>
2023-08-15 22:22:42 +0900

    Change flake8

--- pythonPack/__init__.py
+++ pythonPack/__init__.py
@@ -1,5 +1,6 @@
+# program
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-import checkPath
+import checkPath # noqa
--- pythonPack/checkPath.py
+++ pythonPack/checkPath.py
@@ -4,20 +4,20 @@ import argparse
 
 
 def validPathType(pathCheck: str) -> pathlib.Path:
-    """Custom argparse type for real path"""
-    try:
-        if os.path.isdir(pathCheck) is False:
-            raise ValueError
-        return pathlib.Path(pathCheck)
-    except ValueError:
-        raise argparse.ArgumentTypeError(f"This directory ({pathCheck}) not valid! ")
+  """Custom argparse type for real path"""
+  try:
+    if os.path.isdir(pathCheck) is False:
+      raise ValueError
+    return pathlib.Path(pathCheck)
+  except ValueError:
+    raise argparse.ArgumentTypeError(f"This directory ({pathCheck}) not valid! ")
 
 
 def validFileType(fileCheck: str) -> pathlib.Path:
-    """Custom argparse type for real file"""
-    try:
-        if os.path.isfile(fileCheck) is False:
-            raise ValueError
-        return pathlib.Path(fileCheck)
-    except ValueError:
-        raise argparse.ArgumentTypeError(f"This file ({fileCheck}) not valid! ")
+  """Custom argparse type for real file"""
+  try:
+    if os.path.isfile(fileCheck) is False:
+      raise ValueError
+    return pathlib.Path(fileCheck)
+  except ValueError:
+    raise argparse.ArgumentTypeError(f"This file ({fileCheck}) not valid! ")
